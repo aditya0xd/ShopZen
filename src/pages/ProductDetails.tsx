fb906f2587839fc16ce5c275afd1bb50b24e9b2b
Aditya <adityakryadav10@gmail.com>
2025-12-18 02:26:33 +0530

    Add product details page with dynamic routing

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -71,7 +71,7 @@ export default function ProductDetails() {
           {product.category && <p>Category: {product.category}</p>}
         </div>
 
-        <button className="mt-6 px-6 py-2 bg-black text-white rounded hover:bg-gray-800">
+        <button className="mt-6 px-6 py-2 bg-black text-white rounded hover:bg-gray-700 cursor-pointer">
           Add to Cart
         </button>
       </div>
